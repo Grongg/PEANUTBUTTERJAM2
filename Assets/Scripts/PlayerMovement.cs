3d4bd9882f132849bdf99e92d0a164a4d8e73312
FrostRing <alexandre1.lefevre@epitech.eu>
2020-02-11 20:16:07 +0100

    [ADD] Player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+    }
+}
